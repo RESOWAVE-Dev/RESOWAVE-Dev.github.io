5cfefb126f890272e340bef51ded7c429e55b5fd
vanowarna <55307326+vanowarna@users.noreply.github.com>
2025-06-11 00:11:33 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelectorAll(".fade-in").forEach((el, index) => {
-    setTimeout(() => {
-      el.style.opacity = "1";
-      el.style.transform = "translateY(0)";
-    }, index * 500);
-  });
-});
